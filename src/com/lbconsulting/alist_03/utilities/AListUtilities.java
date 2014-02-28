2268e43e659bb6737d4b081f85b51ea42f542301
LorenBaker <LorenABaker@comcast.net>
2014-02-28 08:13:13 -0800

    DpToPix changes

--- src/com/lbconsulting/alist_03/utilities/AListUtilities.java
+++ src/com/lbconsulting/alist_03/utilities/AListUtilities.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Spinner;
 
@@ -196,5 +197,17 @@ public class AListUtilities {
 		}
 
 		return id;
+	}
+
+	public static int dpToPx(Context context, int dp) {
+		DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+		int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+		return px;
+	}
+
+	public static int pxToDp(Context context, int px) {
+		DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+		int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+		return dp;
 	}
 }
